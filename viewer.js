908e5c955032d9c24b96476b4ba46b13d05a3f33
mijin Lee <halfjin7@gmail.com>
2021-10-13 14:30:44 +0900

    test

--- viewer.js
+++ viewer.js
@@ -49,7 +49,7 @@ const defaultOptions = {
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   defaultUrl: {
-    value: "compressed.tracemonkey-pldi-09.pdf",
+    value: "password-test-doc.pdf",
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
